619ce07bbea0c18b1bf012a4368966c24c6ab685
guckin <donald.stephen.mcguckin@gmail.com>
2019-03-08 19:01:36 -0800

    unit tests

--- spec/slippy_method_hooks_spec.rb
+++ spec/slippy_method_hooks_spec.rb
@@ -5,7 +5,26 @@ RSpec.describe SlippyMethodHooks do
     expect(SlippyMethodHooks::VERSION).not_to be nil
   end
 
-  context 'when .time_box_method is called' do
+  context 'when .time_box_method is used' do
+
+    it 'it does not let the method exceed a given time limit' do
+      timeout_time = 0.5
+      klass = Class.new do
+        include SlippyMethodHooks
+        def meth
+          sleep 1
+        end
+        time_box_method(timeout_time, :meth)
+      end
+      start_time = Time.now
+      begin
+        klass.new.meth
+      rescue
+        end_time = Time.now
+      end
+      result = end_time - start_time
+      expect(result.round(1)).to eq(timeout_time)
+    end
 
     context 'without a block and the method call expires' do
 
@@ -31,7 +50,7 @@ RSpec.describe SlippyMethodHooks do
       end
     end
 
-    context 'is called with a block and the method call expires' do
+    context 'with a block and the method call expires' do
 
       before(:each) do
         expected_result = 'expected-result'
@@ -71,6 +90,94 @@ RSpec.describe SlippyMethodHooks do
         expect(args[0]).to eq(SlippyMethodHooks::TimeoutError)
         expect(args[1]).to be_a(String)
         expect(args[2]).to be_a(Array)
+      end
+    end
+
+    context 'with a block and the method does not expire' do
+      before(:each) do
+        test_class = Class.new do
+          include SlippyMethodHooks
+
+          def self.expected_result
+            'expected-result'
+          end
+
+          attr_reader :args
+
+          def meth
+            sleep 0.1
+            self.class.expected_result
+          end
+
+          time_box_method(0.2, :meth) do |*args|
+            @args = args
+            raise 'bad'
+          end
+        end
+
+        @test_obj = test_class.new
+        @result =
+          begin
+            @test_obj.meth
+          rescue StandardError => e
+            e
+          end
+      end
+
+      it 'returns the result of the method' do
+        expect(@result).to eq(@test_obj.class.expected_result)
+      end
+    end
+    context 'when .rescue_on_fail is called' do
+
+      context 'when no block is provided' do
+
+        before(:each) do
+          @result =
+            begin
+              Class.new do
+                include SlippyMethodHooks
+
+                def meth; end
+
+                rescue_on_fail(:meth)
+              end
+            rescue SlippyMethodHooks::NoBlockGiven => e
+              e
+            end
+        end
+
+        it "raises #{SlippyMethodHooks::NoBlockGiven}" do
+          expect(@result).to be_a(SlippyMethodHooks::NoBlockGiven)
+        end
+      end
+
+      context 'when the method fails' do
+        blk = ->(err = nil) { { test_value: 'test', error: err } }
+
+        before(:each) do
+          @test_class =
+            Class.new do
+              include SlippyMethodHooks
+
+              def meth
+                raise StandardError, 'fart sound'
+              end
+
+              rescue_on_fail(:meth, &blk)
+            end
+        end
+
+        it 'returns the result of the block' do
+          result = @test_class.new.meth
+          expected = blk.call
+          expect(result[:test_value]).to eq(expected[:test_value])
+        end
+
+        it 'yields an error object' do
+          result = @test_class.new.meth
+          expect(result[:error]).to be_a(StandardError)
+        end
       end
     end
   end
